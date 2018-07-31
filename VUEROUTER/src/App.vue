9c0d32399d8a67de92a3316eefca17872b68265d
zhangy <zy123zhangyong@163.com>
2018-07-31 17:58:16 +0800

    vue-router 5_6

--- VUEROUTER/src/App.vue
+++ VUEROUTER/src/App.vue
@@ -1,9 +1,14 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
+    <!--<router-view name="left" style="float:left;width:50%;height:300px;background-color:#ccc;"></router-view>-->
+    <router-link to="/">Home</router-link>|
+    <router-link to="/params/198/jspang website is very good">params</router-link>|
+    <router-link to="/goHome">goHome</router-link>|
+    <router-link to="/goParams/198/jspang website is very good">goParams</router-link>|
+    <!--<router-link to="/goParams/">goParams</router-link>|-->
     <router-view></router-view>
-    <router-view name="left" style="float:left;width:50%;height:300px;background-color:#ccc;"></router-view>
-    <router-view name="right" style="float:left;width:50%;height:300px;background-color:#c0c;"></router-view>
+    <!--<router-view name="right" style="float:left;width:50%;height:300px;background-color:#c0c;"></router-view>-->
   </div>
 </template>
 
--- VUEROUTER/src/router/index.js
+++ VUEROUTER/src/router/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Hi1 from '@/components/Hi1'
 import Hi2 from '@/components/Hi2'
+import Params from '@/components/params'
 
 Vue.use(Router)
 
@@ -10,21 +11,17 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      components: {
-        default:HelloWorld,
-        left:Hi1,
-        right:Hi2,
-      }
+      name: 'HelloWorld'
     },
     {
-      path: '/jspang',
-      name: 'HelloWorld',
-      components: {
-        default:HelloWorld,
-        left:Hi2,
-        right:Hi1,
-      }
+      path:'/params/:newsId(\\d+)/:newsTitle',
+      component:Params
+    },{
+      path:'/goHome',
+      redirect:'/' 
+    },{
+      path:'/goParams/:newsId(\\d+)/:newsTitle',
+      redirect:'/params/:newsId(\\d+)/:newsTitle'  
     }
   ]
 })
