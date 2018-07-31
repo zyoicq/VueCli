5a6dff03fbf32239e74a45599010431b0c81d524
zhangy <zy123zhangyong@163.com>
2018-07-31 09:42:54 +0800

    vue-router 4

---
+++ VUEROUTER/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view></router-view>
+    <router-view name="left" style="float:left;width:50%;height:300px;background-color:#ccc;"></router-view>
+    <router-view name="right" style="float:left;width:50%;height:300px;background-color:#c0c;"></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ VUEROUTER/src/components/Hi2.vue
@@ -0,0 +1,12 @@
+<template>
+    <h2>{{msg}}</h2>
+</template>
+<<script>
+    export default{
+        data(){
+            return {
+                msg:'I am Hi2'
+            }
+        }
+    }
+</script>
